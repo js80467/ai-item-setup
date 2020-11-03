bd050025bcb67f0089172c8b034cbb5759828ae7
Josh <joshua.shell@kroger.com>
2020-11-03 15:26:14 -0500

    test

---
+++ src/main/java/com/kroger/aiitemsetup/AiItemSetupApplication.java
@@ -0,0 +1,13 @@
+package com.kroger.aiitemsetup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiItemSetupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiItemSetupApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kroger/aiitemsetup/AiItemSetupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kroger.aiitemsetup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiItemSetupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
